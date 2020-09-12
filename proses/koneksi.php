0f3a0b25b663c8ae50c5fb4813124bf910325500
Eko Febri Harsono (Maseko) <ekoaewes2006@gmail.com>
2020-09-12 23:14:27 +0700

    first commit

--- proses/koneksi.php
+++ proses/koneksi.php
@@ -2,7 +2,7 @@
  define('HOST','localhost');
  define('USER','root');
  define('PASS','');
- define('DB','parkir_dbserver');
+ define('DB','presensi');
 
  $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
 
--- proses/koneksi_pgs.php
+++
@@ -1,9 +0,0 @@
-<?php
- define('HOST','localhost');
- define('USER','root');
- define('PASS','');
- define('DB','parkir_pgs');
-
- $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
-
- ?>
